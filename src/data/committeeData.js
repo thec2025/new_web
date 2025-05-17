7ca3eb710a49a490466cd90ed1e51717bf2eafc1
Abhinav Saini <abhinavsaini.5106@gmail.com>
2025-05-17 09:56:43 +0530

    small change

--- src/data/committeeData.js
+++ src/data/committeeData.js
@@ -144,7 +144,7 @@ export const uqFellows = [
     {
       name : "Dr. Amanpreet Kaur",
       title : "Associate Professor",
-      department : "EIED., TIET Patiala",
+      department : "ECED., TIET Patiala",
       image: "/images/Dr. Amanpreet.jpg"
     }
   ];
